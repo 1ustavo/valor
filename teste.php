766869e867cf68aa88072769efbaf27c0c7909f0
Gustavo da Silva <gusyavo999@gmail.com>
2024-11-20 20:56:52 -0300

    consulta no banco

--- teste.php
+++ teste.php
@@ -1,23 +1,10 @@
 <?php
-function precificarAplicacao($tipo, $funcionalidades, $tempoEstimado, $custoCasa) {
-    $precoBase = 1000; // Preço inicial
-    $fatoresTipo = [
-        'site' => 1,
-        'ecommerce' => 1.5,
-        'sistema' => 2
-    ];
-    $fatorFuncionalidade = count($funcionalidades) * 200; // R$200 por funcionalidade
+include 'conexao.php';
 
-    $precoFinal = $precoBase * $fatoresTipo[$tipo] + $fatorFuncionalidade + ($tempoEstimado * 100) + $custoCasa;
-    return $precoFinal;
+$consulta1 = $con->query("SELECT * FROM contas");
+while($exibe = $consulta1->fetch(PDO::FETCH_ASSOC)){
+//    var_dump($exibe);
+echo $exibe ['nome'];
 }
 
-// Exemplo de uso
-$tipo = 'ecommerce';
-$funcionalidades = ['carrinho', 'pagamento', 'usuário'];
-$tempoEstimado = 30; // em horas
-$custoCasa = 2000; // Custo extra para trabalhar em casa
-
-$preco = precificarAplicacao($tipo, $funcionalidades, $tempoEstimado, $custoCasa);
-echo "O preço estimado para a aplicação é: R$" . $preco;
 ?>
--- valida.php
+++ valida.php
@@ -1,7 +1,10 @@
 <?php
 session_start();
-$usuario = 'gusta';
-$senha = 1234;
+include 'conexao.php';
+$consulta = $con->query('SELECT * FROM user WHERE id = 1');
+$exibe = $consulta->fetch(PDO::FETCH_ASSOC);
+$usuario = $exibe['nome'];
+$senha = $exibe['senha'];
 
 // echo $_POST['login'];
 // echo $_POST['senha'];
